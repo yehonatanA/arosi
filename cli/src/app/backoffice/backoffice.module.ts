1def33b3dec1236ca4d79c544568c95c5dd92dc1
יהונתן ענבי < >
2017-03-30 16:55:53 +0300

    Headers

--- cli/src/app/backoffice/backoffice.module.ts
+++ cli/src/app/backoffice/backoffice.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { TopComponent } from './top/top.component';
+import { NavbarComponent } from './top/navbar/navbar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavbarComponent],
-  exports: [NavbarComponent]
+  declarations: [NavbarComponent, BreadcrumbsComponent, TopComponent],
+  exports: [NavbarComponent, BreadcrumbsComponent, TopComponent]
 })
 export class BackofficeModule { }
