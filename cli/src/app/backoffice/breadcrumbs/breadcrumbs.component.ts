b728666a86ff31b8585aab10414763c48b7b6929
יהונתן ענבי < >
2017-04-02 03:04:57 +0300

    Breadcrumbs

--- cli/src/app/backoffice/breadcrumbs/breadcrumbs.component.ts
+++ cli/src/app/backoffice/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../top/menu/menu.service';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private menuService: MenuService) { }
+
+  
+  public get selectedMenuItem() : string {
+    return this.menuService.selectedMenuItem;
+  }
+  
 
   ngOnInit() {
   }
