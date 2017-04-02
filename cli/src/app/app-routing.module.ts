c783fdc145f9ca8333f2b0af9bad8b3e3476f927
יהונתן ענבי < >
2017-04-02 03:11:34 +0300

    a

--- cli/src/app/app-routing.module.ts
+++ cli/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule],
     providers: []
 })
