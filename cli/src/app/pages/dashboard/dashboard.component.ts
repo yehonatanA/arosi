b1620dc5826dacc0fae3374f7676596c1b685581
danie <danielamit11@outlook.com>
2017-04-09 03:34:10 +0300

    Create 2 Templats of Orders (Form)

--- cli/src/app/pages/dashboard/dashboard.component.ts
+++ cli/src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SelectItem } from 'primeng/primeng';
 import { Router, RouterModule } from '@angular/router';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,10 +11,13 @@ import { Router, RouterModule } from '@angular/router';
 export class DashboardComponent implements OnInit {
   cities: SelectItem[];
   selectedCity: string;
-cars: Car[];
+Orders :Order[];
+OrderView :OrderView[];
+
   constructor() { 
     this.cities = [];
-    this.cars = [];
+    this.Orders = [];
+    
   }
 
   ngOnInit() {
@@ -24,16 +28,97 @@ cars: Car[];
     this.cities.push({ label: 'Istanbul', value: { id: 4, name: 'Istanbul', code: 'IST' } });
     this.cities.push({ label: 'Paris', value: { id: 5, name: 'Paris', code: 'PRS' } });
 
-    this.cars.push({ vin: 'Paris', year: 1999, brand: 'volvo', color: 'red'});
-    this.cars.push({ vin: 'New-York', year: 2000, brand: 'toyota', color: 'blue'});
-    this.cars.push({ vin: 'Jerusalem', year: 1999, brand: 'volvo', color: 'orange'});
+    this.Orders.push({ OrderNo: '1000', DateOrder: '05/04/17', CustomerName: 'שימי הצב',City: 'רמת-גן',Delivery: 'כן',Quantity:'100',Amount: 2500});
+    this.Orders.push({ OrderNo: '1001', DateOrder: '12/04/17', CustomerName: 'איציק ואהובה',City: 'יהוד' ,Delivery: 'כן',Quantity:'80',Amount: 2000});
+    this.Orders.push({ OrderNo: '1002', DateOrder: '24/04/17', CustomerName: 'שלומי שבתאי',City: 'כפר-סבא', Delivery: 'כן',Quantity:'150',Amount: 3750});
+    
+    
+    
+ /*   this.OrderView.push({
+       OrderNo: '1002', 
+       CreateDate: '24/04/17', 
+       DateOrder: '24/04/17', 
+       CustomerName: 'שלומי שבתאי',
+       City: 'כפר-סבא', 
+       Adress: 'ויצמן 135',
+       PhoneNo: '054-7584255',
+       Hour:'11:30',
+       Type:'ברית',
+       Quantity:'150',
+       PriceOfUnit:'25',
+       Delivery: true,
+       ShippingPrice: '250',
+       
+       DriverName:'150',
+ 
+      });*/
   }
 
+
 }
 
-export interface Car {
-    vin;
-    year;
-    brand;
-    color;
+export interface Order {
+    
+    OrderNo;
+    DateOrder;
+    CustomerName;
+    City;
+    Delivery;
+    Quantity;
+    Amount;
+
+    
 }
+
+export class OrderView {
+    OrderNo ;
+    CreateDate: Date; // תאריך יצירה
+    DateOrder: Date; // תאריך הזמנה
+    CustomerName: String;
+    City: String;
+    Adress: String;
+    PhoneNo;
+    Hour; // לשעה
+    Type; // סוג האירוע
+    Quantity: Number  ; //כמות מנות
+    PriceOfUnit;    //מחיר למנה
+    MainCourse :Food[]; //סוגי עיקריות
+    Extras :Food[]; //תוספות חמות 
+    Salads :Food[]; //סוגי סלטים
+    Pastries:Food[];// גלוב לחוח וסלוף
+    Tools : Tools[]; //כלים וציוד
+    DriverName: String; //נהג
+    Delivery: Boolean; // הובלה
+    ShippingPrice: ShippingPrice; // מחיר הובלה
+    TotalAmount: Number; //  סה"כ
+    UserId; 
+    Comments;
+
+}
+
+export class Tools {
+    Id ;
+    Name: String;
+    Quantity: Number;
+    
+}
+
+
+export class Food {
+    Id ;
+    Category:String;
+    Name: String;
+    Quantity: Number;
+}
+
+
+export class ShippingPrice
+ {
+    Id ;
+    NameCity: String;
+    Price: Number;
+    Distance: Number;
+    Area;
+
+}
+
